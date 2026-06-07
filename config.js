3449d6eba49430f353780a0962012445b5438fca
restplaced-dev <restplaced@gmail.com>
2026-06-07 13:38:25 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ window.HEQILIAO_CONFIG = {
   equipmentCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSepWHrHrBa3VZJAL2d0Ga8jOa2_qpZAu6cHNiY7aBYhmMY_i0rK-NfGEGQmSgYyoBWQUHhysZm9KVe/pub?gid=376213302&single=true&output=csv",
 
   // 硬體販售 CSV：建立「硬體販售」分頁並發布 CSV 後，把網址貼到這裡
-  hardwareSaleCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSepWHrHrBa3VZJAL2d0Ga8jOa2_qpZAu6cHNiY7aBYhmMY_i0rK-NfGEGQmSgYyoBWQUHhysZm9KVe/pub?gid=858285018&single=true&output=csv",
+  hardwareSaleCsvUrl: "",
 
   questionnaireUrl: "https://docs.google.com/forms/d/e/1FAIpQLSe27SlEwZQrY_D3Zv_-TUr2Xwg0ImgzkrwHCTbeB9ByuVS0YA/viewform",
   quarantineRecordUrl: "https://docs.google.com/document/d/1vH2a4YFpSMDVqkEpPS6Y58D4G45ALnZhn51a7fMO6l4/edit?tab=t.ib81wg9n3ayj",
